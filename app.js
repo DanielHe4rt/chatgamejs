ecde8ad73908b49b25cad8ab9cd654802fa1822f
Daniel Reis <daniel_o_amigo@hotmail.com>
2017-12-14 20:20:02 -0200

    Integração Client/Server side
    Funcionamento do Socket.io está 100%, porém não tem nenhum tipo de jogador até o momento

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express')
+var app = express();
+var serv = require('http').Server(app)
+var SOCKET_LIST = {};
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/client/index.html')
+})
+app.use('/client',express.static(__dirname + '/client'))
+
+serv.listen(2000);
+console.log("Servidor Online!")
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection',function(socket){
+	console.log("Jogador Conectado!");
+	socket.id = Math.random();
+	socket.x = 0;
+	socket.y = 0;
+	SOCKET_LIST[socket.id] = socket;
+	socket.on('disconnect',function(){
+		delete SOCKET_LIST[socket.id];
+	})
+})
+
+setInterval(function(){
+	var pacote = []
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;
+		pacote.push({
+			x: socket.x,
+			y: socket.y
+		});
+	}
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('posicoes',pacote);
+	}
+},1000/25);
