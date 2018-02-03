7ea1d03171666485e77ab5fb42b35f8912877f1e
Daniel Reis <daniel_o_amigo@hotmail.com>
2018-02-03 15:48:24 -0200

    Movimentação do jogador
    Movimentação do jogador.

--- app.js
+++ app.js
@@ -9,17 +9,28 @@ app.use('/client',express.static(__dirname + '/client'))
 
 serv.listen(2000);
 console.log("Servidor Online!")
-
+let playerSpeed = 10;
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection',function(socket){
 	console.log("Jogador Conectado!");
 	socket.id = Math.random();
-	socket.x = 0;
-	socket.y = 0;
+	socket.x = 250;
+	socket.y = 250;
 	SOCKET_LIST[socket.id] = socket;
+	socket.on('movement',function(data){
+		console.log(data);
+		if(data.tecla === "s" || data.tecla === "S"){
+			socket.y += playerSpeed;
+		}else if(data.tecla === "W" || data.tecla === "w"){
+			socket.y -= playerSpeed;
+		}else if(data.tecla === "d" || data.tecla === "D"){
+			socket.x += playerSpeed;
+		}else if(data.tecla === "a" || data.tecla === "A"){
+			socket.x -= playerSpeed;
+		}
+	});
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
-		
 	})
 })
 
@@ -27,8 +38,7 @@ setInterval(function(){
 	var pacote = []
 	for(var i in SOCKET_LIST){
 		var socket = SOCKET_LIST[i];
-		socket.x++;
-		socket.y++;
+		
 		pacote.push({
 			x: socket.x,
 			y: socket.y
